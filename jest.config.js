c36e74415c464c6c84f9dd6ae9eb8b8df71d39d7
brent <brent@n3f.com>
2025-07-26 16:47:37 -0700

    Fix jest.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
 	testPathIgnorePatterns: ['/node_modules/', '/specs/'],
 	testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.spec.js'],
 	testEnvironment: 'jsdom',
