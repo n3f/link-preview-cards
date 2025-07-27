88090553e3d7cf2c61cacec78cc43be4cdc746cd
brent <brent@n3f.com>
2025-07-26 18:15:31 -0700

    Remove build directory.

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cd8984bb88ae0bf7df0f');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'd517a124765374564299');
